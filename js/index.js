99b946d7b9fc2e36344a95e6faff48e3229d56ae
nime0110 <77383149+nime0110@users.noreply.github.com>
2021-07-20 11:20:47 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,6 +37,18 @@ $(function(){
       $('html,body').stop().animate({scrollTop:target},1500)
     }
   })
+  $('#gnb>ul>li').on('mouseover',function(){
+    $(this).find('.sub').stop().slideDown(600);
+  })
+  $('#gnb>ul>li').on('mouseleave',function(){
+    $(this).find('.sub').stop().slideUp(600);
+  })
+  $('.sub').hide();
+
+
+
+
+
 
 
 })
